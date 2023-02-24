66a9255aa3a7762cbc0100bcb1500e02a42c6ac2
Maxim Wilman <mwilman@brockhaus-ag.de>
2023-02-24 16:01:06 +0100

    fixed pasta component

--- src/app/menu-card/pasta/pasta.component.ts
+++ src/app/menu-card/pasta/pasta.component.ts
@@ -1,21 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Product} from "../types/product";
 import {ProductService} from "../product.service";
+import {map, Observable, of} from "rxjs";
 
 @Component({
   selector: 'app-pasta',
   templateUrl: './pasta.component.html',
   styleUrls: ['./pasta.component.scss']
 })
-export class PastaComponent {
-  products: Product[] = [];
+export class PastaComponent implements OnInit {
+  products$: Observable<Product[]> = of([]);
+  selectedProduct: Product[] = [];
+
 
   constructor(private productService: ProductService) {
   }
 
   ngOnInit() {
-    this.productService.getProducts().subscribe(
-      (data) => this.products = data.filter((p) => p.type === 'pasta')
-    );
+    this.products$ = this.productService.getProducts()
+      .pipe(
+        map((products) => {
+          return products.filter((item) => item.type === 'pasta');
+        })
+      );
+  }
+
+  selectProduct(product: Product) {
+    console.log(product.description);
+    this.productService.addToCart(product);
+    //this.messageService.add({severity:'info', summary:'Product Selected', detail: product.name});
+  }
+
+  onRowSelect(event: any) {
+    console.log(event.data.description);
+    this.productService.addToCart(event.data);
+    //this.messageService.add({severity:'info', summary:'Product Selected', detail: event.data.name});
   }
 }
